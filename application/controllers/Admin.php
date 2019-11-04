eda948f206a969668a6a58a6a276e1ff40a6451d
debyprast <debyprast18@gmail.com>
2019-11-05 01:22:50 +0700

    Membuat class index admin dan controller admin buat load

---
+++ application/controllers/Admin.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper(array('form', 'url'));
+    }
+	public function index()
+	{
+		$this->load->view('template_admin/header');
+		$this->load->view('template_admin/sidebar');
+		$this->load->view('admin/index');
+		$this->load->view('template_admin/footer');
+    }
+}
