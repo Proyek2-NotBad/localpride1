d2a9c550410899cfc1836a2797a299b06f69f97d
debyprast <debyprast18@gmail.com>
2019-11-07 21:47:41 +0700

    penghapusan navbar di setiap kelas agar tidak tumpang tindih

--- application/views/home/baju.php
+++ application/views/home/baju.php
@@ -1,30 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  
-
-  <!-- Main Stylesheet File -->
-  <link href="css/style.css" rel="stylesheet"><header id="header">
-    <div class="container">
-
-      <div id="logo" class="pull-left">
-        <a href="index.html"><img src="<?= base_url('assets_web/') ?>img/log.png" alt="" title="" /></img></a>
-        <!-- Uncomment below if you prefer to use a text image -->
-        <!--<h1><a href="#hero">Header 1</a></h1>-->
-      </div>
-
-      <nav id="nav-menu-container">
-        <ul class="nav-menu">
-					<li class="active"><a href="<?= base_url(); ?>Home/baju">  Baju</a></li>
-					<li class="active"><a href="<?= base_url(); ?>Home/tas">  Tas</a></li>
-					<li class="active"><a href="<?= base_url(); ?>Home/sepatu"> Sepatu </a></li>
-					<li class="active"><a href="<?= base_url(); ?>Home/kontak"> Kontak </a></li>
-          <li class="active"><a href="<?= base_url(); ?>Home/tentang">  Tentang Kami</a></li>
-      </nav>
-      <!-- #nav-menu-container -->
-    </div>
-  </header>
 
   <div id="team" class="our-team-area area-padding">
         <div class="container">
