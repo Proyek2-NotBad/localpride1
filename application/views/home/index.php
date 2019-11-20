0a1b3dd3a6294b5a06614a282ee39e75a2651630
debyprast <debyprast18@gmail.com>
2019-11-20 09:55:14 +0700

    memperbaiki halaman home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,16 +1,19 @@
-
-            <div class="container">			
+<div id="team" class="our-team-area area-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="section-headline text-center">
+                        <h2>Menu Kami</h2>
+                    </div>
+                </div>
+            </div>
+        
+		<div class="container">			
 		
 		<!-- akhir jumbotron -->
 		<div class="col-sm-6 col-md-3">
 			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/21.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0921</h3>
-					<p>Harga 90.000</p>
-					<p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
+			<a href="<?= base_url(); ?>Home/baju"><img src="<?= base_url('assets_web/') ?>img/1.jpg" alt="" title="" /></a>
+        	</div>
 		</div>
 		</div>
