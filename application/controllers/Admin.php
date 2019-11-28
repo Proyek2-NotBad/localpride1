2b1d68b23132f9216e73edc1fff3887c8c14c7c3
debyprast <debyprast18@gmail.com>
2019-11-28 22:33:41 +0700

    proses backend

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -7,6 +7,9 @@ class Admin extends CI_Controller {
     {
         parent::__construct();
         $this->load->helper(array('form', 'url'));
+        $this->load->library('form_validation');
+
+		    $this->load->model("Model_baju");
     }
     
     public function index(){
@@ -15,11 +18,12 @@ class Admin extends CI_Controller {
 		$this->load->view('admin/index');
 		$this->load->view('template_admin/footer');
     }
-
-    public function tambahbaju(){
-		$this->load->view('template_admin/header');
+    
+    public function tambahbaju(){                        
+    $data["baju"] = $this->Model_baju->getAll();
+    $this->load->view('template_admin/header');
 		$this->load->view('template_admin/sidebar');
-		$this->load->view('admin/tambah_baju');
+		$this->load->view('admin/tambah_baju', 'data');
 		$this->load->view('template_admin/footer');
     }
 
@@ -35,5 +39,32 @@ class Admin extends CI_Controller {
     $this->load->view('template_admin/sidebar');
     $this->load->view('admin/tambah_tas');
     $this->load->view('template_admin/footer');
+    }
+
+    public function bajuadd()
+    {
+        $tambah = $this->Model_baju;
+            $tambah->save();
+			$this->session->set_flashdata('success', 'Berhasil disimpan');
+			redirect('Admin/tambahbaju');        
+    }
+
+    public function bajudelete($id_baju = null)
+    {
+        if (!isset($id_baju)) show_404();
+
+		
+        if ($this->Model_baju->delete($id_baju)) {
+			redirect('Admin/tambahbaju');
+        }
+  }
+  
+  public function databaju(){
+      $data["baju"] = $this->Model_baju->getAll();
+		$this->load->view('template_admin/header');
+		$this->load->view('template_admin/sidebar');
+		$this->load->view('admin/tambahbaju', $data);
+		$this->load->view('template_admin/footer');
+        
     }
 }
