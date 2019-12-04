e7d4b781172a0585a6b17d8b8e8b8ae1e9099895
debyprast <debyprast18@gmail.com>
2019-12-04 10:18:12 +0700

    perbaikan

--- application/views/home/baju.php
+++ application/views/home/baju.php
@@ -18,7 +18,7 @@
             <img src="<?= base_url('assets_web/') ?>img/1.jpg" alt="">
 				<div class="caption">
 					<h3>Kode product 0921</h3>
-					<p>Harga 90.000</p>
+					<p>Harga 95.000</p>
 					<p>Ready Stok 3</p>
 					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
 				</div>
