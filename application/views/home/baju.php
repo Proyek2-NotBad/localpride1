396299cec3fa20d17ba1bf45b39ccb1c73fba442
debyprast <debyprast18@gmail.com>
2019-12-31 22:52:58 +0700

    Edit Index Baju,Jaket,Polo,Tas,Sepatu & Sandal

--- application/views/home/baju.php
+++ application/views/home/baju.php
@@ -13,100 +13,19 @@
             <div class="container">			
 		
 		<!-- akhir jumbotron -->
+		<?php foreach($listbaju->result_object() as $data) : ?>
 		<div class="col-sm-6 col-md-3">
 			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/1.jpg" alt="">
+            <img src="<?= base_url('foto/user/'.$data->foto) ?>" alt="">
 				<div class="caption">
 					<h3>Kode product 0211</h3>
-					<p>Harga 85.000</p>
-					<p>Ready Stok 4</p>
+					<p>Harga <?= $data->harga ?></p>
+					<p>Ready Stok <?= $data->stok; ?></p>
 					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
 				</div>
 			</div>
 		</div>
-
-        <div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/2.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0222</h3>
-					<p>Harga 100.000</p>
-					<p>Ready Stok 6</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
-
-        <div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/3.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0223</h3>
-					<p>Harga 70.000</p>
-					<p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
-
-        <div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/4.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0224</h3>
-					<p>Harga 100.000</p>
-					<p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
- 
-		<div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/5.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0225</h3>
-					<p>Harga 90.000</p>
-					<p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
-
-        <div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/6.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0226</h3>
-					<p>Harga 97.000</p>
-					<p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
-
-        <div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/7.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0227</h3>
-					<p>Harga 85.000</p>
-					<p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
-
-        <div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/8.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0228</h3>
-					<p>Harga 90.000</p>
-          			<p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
+		<?php endforeach; ?>
+        
 	</div>
     </div>
--- application/views/home/sepatu.php
+++ application/views/home/sepatu.php
@@ -12,101 +12,19 @@
 
             <div class="container">			
 
+		<?php foreach($listsepatu->result_object() as $data) : ?>
 		<!-- akhir jumbotron -->
 		<div class="col-sm-6 col-md-3">
 			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/18.jpg" alt="">
+            <img src="<?= base_url('foto/user/'.$data->foto) ?>" alt="">
 				<div class="caption">
 					<h3>Kode product 0331</h3>
-					<p>Harga 90.000</p>
-					<p>Ready Stok 3</p>
+					<p>Harga <?= $data->harga ?></p>
+					<p>Ready Stok <?= $data->stok; ?></p>
 					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
 				</div>
 			</div>
 		</div>
-
-        <div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/18.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0332</h3>
-					<p>Harga 90.000</p>
-					<p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
-
-        <div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/18.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0921</h3>
-					<p>Harga 90.000</p>
-					<p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
-
-        <div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/18.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0921</h3>
-					<p>Harga 90.000</p>
-					<p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
- 
-		<div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/18.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0921</h3>
-					<p>Harga 90.000</p>
-					<p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
-
-        <div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/18.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0921</h3>
-					<p>Harga 90.000</p>
-					<p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
-
-        <div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/18.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0921</h3>
-					<p>Harga 90.000</p>
-					<p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
-
-        <div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-            <img src="<?= base_url('assets_web/') ?>img/18.jpg" alt="">
-				<div class="caption">
-					<h3>Kode product 0921</h3>
-					<p>Harga 90.000</p>
-          <p>Ready Stok 3</p>
-					<p><a href="<?= base_url(); ?>Home/detail" class="btn btn-primary" role="button">Lihat</a></p>
-				</div>
-			</div>
-		</div>
+			<?php endforeach; ?>
 	</div>
     </div>
